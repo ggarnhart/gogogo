b27414698605cd064db0b01c22e121604d8c5223
Greg Garnhart <greg.garnhart12@gmail.com>
2025-12-24 23:38:25 -0500

    health

--- internal/handlers/health.go
+++ internal/handlers/health.go
@@ -8,5 +8,5 @@ import (
 
 func HealthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(map[string]string{"status": "ok", "time": time.Now().Format(time.RFC3339)})
+	json.NewEncoder(w).Encode(map[string]string{"status": "ok", "time": time.Now().Format(time.RFC3339), "version": "0.0.1"})
 }
